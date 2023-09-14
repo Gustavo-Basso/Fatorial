619b1bad1d666288ec03938c7d85ced999f954a4
Gustavo Basso <gustavo.basso01@hotmail.com>
2023-09-14 02:36:31 -0300

    First commit

---
+++ Entities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatorialPj {
+    internal class Entities {
+
+        public int N { get; set; }
+        
+
+        public Entities(int n) {
+
+            N = n;
+
+        }
+
+        public int Fatorial(int n) {
+
+            int sum = 1;
+
+            for (int i = 1; i <= N; i++) {
+
+
+                sum += sum * (n - i);
+                
+            }
+            return sum;
+        
+        }
+
+        public override string ToString() {
+            return Fatorial(N).ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FatorialPj {
+
+    internal class Program {
+
+
+        static void Main(string[] args) {
+
+
+            Console.Write("Factorial valor:  ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Entities fac = new Entities(n);
+            Console.WriteLine(fac);
+
+
+        }
+
+
+    }
+
+}
